1bab923988a5c85b95e91a51465af94b0b7675d3
18230086651 <18230086651@163.com>
2018-08-01 21:15:56 +0800

    首页

---
+++ js/index.js
@@ -0,0 +1,11 @@
+var swiper = new Swiper('.swiper-container', {
+    slidesPerView: 3,
+    spaceBetween: 10,
+    loop: true,
+    // width: 348,
+
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev'
+    }
+})
